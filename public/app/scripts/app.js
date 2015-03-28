b285a44d9977993983b4ee0d82bedae426192eda
GuiLLeRm0 <gdavid.ptorrez@gmail.com>
2015-03-28 19:28:12 -0400

    Controllers

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -4,4 +4,14 @@ angular.module('chatApp', ['ngRoute'])
 
 .config(function ($routeProvider){
 
+    $routeProvider
+    .when('/chat-rooms', {
+        templateurl : 'partials/chat-rooms.html',
+        controller : 'ChatRoomsCtrl'
+    })
+    .when('/chat-room', {
+        templateurl : 'partials/chat-room.html',
+        controller : 'ChatRoomCtrl'
+    });
+
 });
---
+++ public/app/scripts/controllers/chat-rooms.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('chatApp')
+
+.controller('ChatRoomsCtrl', function($scope){
+
+});
